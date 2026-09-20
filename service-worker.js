a394d4459c9923963dffc260f8b6aef4b240cc45
Bastian Correa <57375170+bcorrv@users.noreply.github.com>
2026-09-20 09:54:31 -0300

    Refresh PWA cache for v0.3

--- service-worker.js
+++ service-worker.js
@@ -1,5 +1,35 @@
-const CACHE='root-sandbox-v02';
-const ASSETS=['./','index.html','manifest.webmanifest','icon.svg'];
-self.addEventListener('install',e=>e.waitUntil(caches.open(CACHE).then(c=>c.addAll(ASSETS))));
-self.addEventListener('activate',e=>e.waitUntil(self.clients.claim()));
-self.addEventListener('fetch',e=>e.respondWith(caches.match(e.request).then(r=>r||fetch(e.request))));
+const CACHE='root-rpg-v03';
+const ASSETS=[
+  './',
+  './index.html',
+  './styles.css',
+  './app.js',
+  './manifest.webmanifest',
+  './icon.svg',
+  './forest-silhouette.svg',
+  './woodland-pattern.svg'
+];
+
+self.addEventListener('install',event=>{
+  event.waitUntil(caches.open(CACHE).then(cache=>cache.addAll(ASSETS)));
+  self.skipWaiting();
+});
+
+self.addEventListener('activate',event=>{
+  event.waitUntil(
+    caches.keys().then(keys=>Promise.all(keys.filter(k=>k!==CACHE).map(k=>caches.delete(k))))
+      .then(()=>self.clients.claim())
+  );
+});
+
+self.addEventListener('fetch',event=>{
+  event.respondWith(
+    fetch(event.request)
+      .then(response=>{
+        const copy=response.clone();
+        caches.open(CACHE).then(cache=>cache.put(event.request,copy)).catch(()=>{});
+        return response;
+      })
+      .catch(()=>caches.match(event.request).then(r=>r||caches.match('./index.html')))
+  );
+});
